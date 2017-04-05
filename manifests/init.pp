04270df3a1fa40d6830976bca153e61eee180bf9
Oscar Aguilera <oscar_aguilera_huerta@hotmail.com>
2017-04-05 17:26:29 -0500

    Change on Init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -42,7 +42,52 @@
 #
 # Copyright 2017 Your name here, unless otherwise noted.
 #
-class mavenfixed {
+class maven { #Maven Class
+  $maven_home = "/usr/lib/maven"
+  $maven_archive = "apache-maven-3.3.9-bin.tar.gz"
+  $maven_folder = "apache-maven-3.3.9"
+
+  exec{'get_maven':
+  command => "/usr/bin/wget -q http://www-eu.apache.org/dist/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.tar.gz  -O /tmp/apache-maven-3.3.9-bin.tar.gz",
+}
 
 
+  Exec {
+      path => [ "/usr/bin", "/bin", "/usr/sbin"]
+  }
+
+
+file { "/tmp/apache-maven-3.3.9-bin.tar.gz":
+      ensure => present,
+      source => 'puppet:///modules/maven/apache-maven-bin.tar.gz',
+      owner => root,
+      mode => '0755',
+           require => Exec["get_maven"],
+  }
+
+
+  exec { 'extrac_ maven',
+      cwd => "/tmp",
+      command => "tar -xfv /tmp/apache-maven-3.3.9-bin.tar.gz",
+      creates => "${maven_home}",
+      require => File["/tmp/$maven_archive"]
+  }
+
+  exec { 'move_maven',
+      command => "mv ${maven_folder} ${maven_home}",
+      creates => "${maven_home}",
+      cwd => cd "/tmp",
+      require => Exec["extract maven"]
+  }
+
+  file { "/etc/profile.d/maven.sh":
+      content =>  "export MAVEN_HOME=${maven_home}
+                   export M2=\$MAVEN_HOME/bin
+                   export PATH=\$PATH:\$M2
+                   export MAVEN_OPTS=\"-Xmx512m -Xms512m\""
+  }
+
+  exec { 'maven_shell'
+
+  }
 }
